8a28a1240b854754fc1b6729d112f8b4b7986e4a
esseun <esseun@gmail.com>
2016-05-05 11:39:52 -0700

    Added more loading logic to let user know that work is being done

--- www/src/nfc/nfc.controller.js
+++ www/src/nfc/nfc.controller.js
@@ -40,52 +40,11 @@ wittsNfc.factory('NfcService', function($rootScope, $ionicPlatform, $ionicLoadin
                 payload = $filter('decodePayload')(tagData.tag.ndefMessage[0]);
                 tagData.patientId = payload[0];
                 tagData.measuredTemp = payload[1];
-
-                PatientRecordsService.getPatientInfo(tagData.patientId)
-                    .then(
-                        function(response) {
-                            tagData.patientInfo = response.data
-                            $ionicLoading.hide();
-                        },
-                        function(httpError) {
-                            $ionicLoading.hide();
-                            $ionicPopup.alert({
-                                title: 'Error connecting to server - please try again'
-                            }).then(function(res) {
-                                angular.copy({}, tagData);
-                            });
-                            throw httpError.status + " : " +
-                                httpError.data;
-                        });
-
-                PatientRecordsService.getPatientTempRecords(tagData.patientId)
-                    .then(
-                        function(response) {
-                            for (i = response.data.length; i > 0; i--) {
-                                if (i == response.data.length - 5) {
-                                    // by default display 5 most recent measurements
-                                    break;
-                                }
-                                timeEntered.unshift($filter('date')(response.data[i-1].timeEntered, 'yyyy-MM-dd @h:mma'));
-                                tempFahrenheit.unshift(response.data[i-1].tempFahrenheit);
-                            }
-                            patientTempRecords.timeEntered = timeEntered;
-                            patientTempRecords.tempFahrenheit = tempFahrenheit;
-                            $ionicLoading.hide();
-                        },
-                        function(httpError) {
-                            $ionicLoading.hide();
-                            $ionicPopup.alert({
-                                title: 'Error connecting to server - please try again'
-                            }).then(function(res) {
-                                angular.copy({}, tagData);
-                            });
-                            throw httpError.status + " : " +
-                                httpError.data;
-                        });
+                console.log("Tag: ", tagData.tag);
+                console.log("PatientId: ", tagData.patientId);
+                console.log("Measured Temperature: ", tagData.measuredTemp);
+                getPatientInfo(tagData.patientId);
             });
-            console.log("Tag: ", tagData.tag);
-            console.log("PatientId: ", tagData.patientId);
         }, function() {
             console.log("Listening for NDEF Tags.");
         }, function(reason) {
@@ -93,10 +52,77 @@ wittsNfc.factory('NfcService', function($rootScope, $ionicPlatform, $ionicLoadin
         })
     });
 
+    function getPatientInfo(patientId) {
+        PatientRecordsService.getPatientInfo(patientId)
+            .then(
+                function(response) {
+                    if (response.data.length) {
+                        tagData.patientInfo = response.data;
+                        getPatientTempRecords(patientId);
+                    } else {
+                        $ionicLoading.hide();
+                        // patient doesn't exist in records database
+                        $ionicPopup.alert({
+                            title: 'Patient ID does not exist - please contact the system administrator at esseun@gmail.com'
+                        }).then(function(res) {
+                            angular.copy({}, tagData);
+                        });
+                    }
+                },
+                function(httpError) {
+                    $ionicLoading.hide();
+                    $ionicPopup.alert({
+                        title: 'Error connecting to server - please try again'
+                    }).then(function(res) {
+                        angular.copy({}, tagData);
+                    });
+                    throw httpError.status + " : " +
+                        httpError.data;
+                });
+    };
+
+    function getPatientTempRecords(patientId) {
+        PatientRecordsService.getPatientTempRecords(patientId)
+            .then(
+                function(response) {
+                    if (response.data.length) {
+                        for (i = response.data.length; i > 0; i--) {
+                            if (i == response.data.length - 5) {
+                                // by default display 5 most recent measurements
+                                break;
+                            }
+                            timeEntered.unshift($filter('date')(response.data[i-1].timeEntered, 'yyyy-MM-dd @h:mma'));
+                            tempFahrenheit.unshift(response.data[i-1].tempFahrenheit);
+                        }
+                        patientTempRecords.timeEntered = timeEntered;
+                        patientTempRecords.tempFahrenheit = tempFahrenheit;
+                        $ionicLoading.hide();
+                    } else {
+                        $ionicLoading.hide();
+                        $ionicPopup.alert({
+                            title: 'No previous temperature measurements found'
+                        }).then(function(res) {
+                            angular.copy({}, tagData);
+                        });
+                    }
+                },
+                function(httpError) {
+                    $ionicLoading.hide();
+                    $ionicPopup.alert({
+                        title: 'Error connecting to server - please try again'
+                    }).then(function(res) {
+                        angular.copy({}, tagData);
+                    });
+                    throw httpError.status + " : " +
+                        httpError.data;
+                });
+    };
+
     return {
         tagData: tagData,
         patientTempRecords: patientTempRecords,
         postPatientTempRecords: function(patientId, tempF) {
+            $ionicLoading.show();
             PatientRecordsService.postPatientTempRecords(patientId, tempF)
                 .then(
                     function(rseponse) {
@@ -105,8 +131,10 @@ wittsNfc.factory('NfcService', function($rootScope, $ionicPlatform, $ionicLoadin
                         }).then(function(res) {
                             angular.copy({}, tagData);
                         });
+                        $ionicLoading.hide();
                     },
                     function(httpError) {
+                        $ionicLoading.hide();
                         $ionicPopup.alert({
                             title: 'Error connecting to server - please try again'
                         }).then(function(res) {
